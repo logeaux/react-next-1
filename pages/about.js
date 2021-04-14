022f05b6dcfb477fca0a98a5a5d2c703e2466532
logeaux <36168460+logeaux@users.noreply.github.com>
2021-04-14 14:11:56 -0500

    Final

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="about" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet omnis maiores et similique ipsam quibusdam, pariatur harum quidem explicabo recusandae eveniet dolores delectus iure voluptate rem ex magni deserunt sunt!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet omnis maiores et similique ipsam quibusdam, pariatur harum quidem explicabo recusandae eveniet dolores delectus iure voluptate rem ex magni deserunt sunt!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet omnis maiores et similique ipsam quibusdam, pariatur harum quidem explicabo recusandae eveniet dolores delectus iure voluptate rem ex magni deserunt sunt!</p>
+      </div>
+    </>
+  );
+}
+
+export default About;
